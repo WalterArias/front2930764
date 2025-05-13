4185354b9947c5057c37961667730e2abdb87843
walterArias <walter.arias.aguirre@gmail.com>
2025-05-13 13:13:05 -0500

    proyectos de clase

--- 10-MODULES/matematicas/readme.md
+++
@@ -1,3 +0,0 @@
-# documentacion del proyecto
-
-package.json : contiene la lista de las dependencias de paquetes de mi proyecto
--- 10-MODULES/matematicas/src/main.js
+++
@@ -1,5 +0,0 @@
-//import * as mate from "./matematicas.js";
-import { restar, PI } from "./matematicas.js";
-
-console.log(restar(8, 3));
-//console.log(mate.restar(8, 3));
